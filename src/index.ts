7baa38a9bbb4bb777901a7a5987a006cbe50b5a8
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-05-27 19:51:56 +0630

    debug(app): remove console.clear

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,6 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-console.clear();
-
 https.globalAgent = new Agent({ keepAlive: true });
 
 const exitOnCancel = (state: any) => {
